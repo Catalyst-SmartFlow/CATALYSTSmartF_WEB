efe7d3171ccbeb5ec00d7daa2f382c31d4295f2b
Jhongdlp <jhongdlp204@gmail.com>
2025-12-09 23:46:00 -0500

    feat: Introduce SalesProblem component and refactor InvisibleWorkforceV3 content and visual animations.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import PartnersCarousel from "@/components/home/PartnersCarousel";
 import InvisibleWorkforceV3 from "@/components/home/InvisibleWorkforceV3";
 import CatalystAdvantage from "@/components/home/CatalystAdvantage";
 import { SalesTeamHeadline } from "@/components/home/SalesTeamHeadline";
+import { SalesProblem } from "@/components/home/SalesProblem";
 import IntegrationsCarousel from "@/components/home/IntegrationsCarousel";
 import StickyScrollFeatures from "@/components/home/StickyScrollFeatures";
 import MobileFeatures from "@/components/home/MobileFeatures";
@@ -21,6 +22,7 @@ export default function Home() {
 
             {/* Sección de Fuerza Laboral Invisible (Velocity Engine V3) */}
             <SalesTeamHeadline />
+            <SalesProblem />
 
             {/* Sección de Fuerza Laboral Invisible (Velocity Engine V3) */}
             <InvisibleWorkforceV3 />
